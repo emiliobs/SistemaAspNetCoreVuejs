01bb3f5be7ceb64ef5e4935c8162e711a3a46f83
Emilio Barrera <barrera_emilio@hotmail.com>
2018-11-22 21:10:39 +0000

    categoriaController

--- Sistema.Entidades/Almacen/Categoria.cs
+++ Sistema.Entidades/Almacen/Categoria.cs
@@ -1,19 +1,26 @@
-﻿namespace Sistema.Entidades.Almacen
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sistema.Entidades.Almacen
 {
     using System.ComponentModel.DataAnnotations;
 
 
     public class Categoria
     {
+        [Column("idcategoria")]
         public int CategoriaId { get; set; }
 
         [Required]
         [StringLength(50, MinimumLength = 3,ErrorMessage = "El Nombre no debe tener más de 50 caracteres, ni menos de 3 caracters")]
+
+        [Column("nombre")]
         public string Nombre { get; set; }
 
+        [Column("descripcion")]
         [StringLength(256)]
         public string Descripcion { get; set; }
 
+        [Column("condicion")]
         public bool Condicion { get; set; }
     }
 }
